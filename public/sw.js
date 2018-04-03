fead84752d9961925aaa591eb7f0eb9004956465
Souradeep Nanda <souradeep_nanda@mckinsey.com>
2018-04-03 09:41:28 +0530

    Cache first strategy update

--- public/sw.js
+++ public/sw.js
@@ -3,7 +3,7 @@ const CACHE = 'my-cache';
 self.addEventListener('install', (event) => {
   console.log('The service worker is being installed.');
 
-  // event.skipWaiting();
+  caches.delete(CACHE);
 });
 
 const fromCache = async (request) => {
@@ -20,21 +20,32 @@ const fromNetwork = async (request) => {
 const cacheFirstStrategy = async (request) => {
   const result = await fromCache(request);
   if (result) {
-    return result.clone();
+    return result;
   }
-  const networkResponse = await fromNetwork(request);
-  const cache = await caches.open(CACHE);
+  const networkPromise = fromNetwork(request);
+  const cachePromise = caches.open(CACHE);
+  const [networkResponse, cache]
+    = await Promise.all([networkPromise, cachePromise]);
   await cache.put(request, networkResponse.clone());
-  return networkResponse.clone();
+  return networkResponse;
+};
+
+const shouldRouteBeIntercepted = (url) => {
+  const isBlacklisted =
+    /sockjs-node/.test(url) ||
+    /chrome-extension/.test(url) ||
+    /hot-update/.test(url);
+  return !isBlacklisted;
 };
 
 self.addEventListener('fetch', (event) => {
-  if (event.request.url.match(/info/)) {
+  // Some react monitoring route, ignore this
+  if (!shouldRouteBeIntercepted(event.request.url)) {
     event.respondWith(fromNetwork(event.request));
     return;
   }
 
-  console.log('The service worker is serving the asset.', event.request);
+  console.log('The service worker is serving the asset.', event.request.url);
 
   event.respondWith(cacheFirstStrategy(event.request));
 });
